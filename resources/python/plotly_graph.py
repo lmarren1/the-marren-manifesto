20797a36a3082aee314976b8fcb055d29e09716d
Luke Marren <lukemarren173@gmail.com>
2024-08-07 18:13:14 -0500

    Add docstring to plotly_graph.py and time_logger_analysis

---
+++ resources/python/plotly_graph.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Name: plotly_graph
+
+Version:
+	0.0.1
+
+Summary:
+    This script produces a plottly graph to visualize the CSV files produced for the 10k mastery tracker project along with their analysis.
+
+Author:
+    Luke Marren
+
+License: MIT
+
+Requires:
+
+Date Last Modified:
+	August 07, 2024
+"""
