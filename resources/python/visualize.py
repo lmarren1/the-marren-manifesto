c0f6e0b7c5fefc004119ae229c5d1acf45453d04
Luke Marren <lukemarren173@gmail.com>
2024-08-12 17:41:47 -0500

    Create bare-bones visualization script

---
+++ resources/python/visualize.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Name: 
+    visualize
+
+Version:
+	0.0.1
+
+Summary:
+    This script produces plotly graphs visualizing my progress on the 10k mastery tracker project.
+
+Author:
+    Luke Marren
+
+License: 
+    MIT
+
+Requires:
+    load_data
+
+Date Last Modified:
+	August 12, 2024
+"""
+
+import load_data
+
+
+def visualize_data():
+    pass
+
+
+def main() -> None:
+    """
+    Main function to generate analysis variables and their respective summary statistics.
+    """
+    load_data()
+    visualize_data()
+
+
+if __name__ == "__main__":
+    main()
