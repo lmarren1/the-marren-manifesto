3f761912919fd64587a58ee206ef812126fddc21
Luke Marren <lukemarren173@gmail.com>
2024-08-13 19:17:19 -0500

    Create first plotly graph to embed in index.html

--- resources/python/visualize.py
+++ resources/python/visualize.py
@@ -6,7 +6,7 @@ Name:
     visualize
 
 Version:
-	0.0.2
+	0.0.3
 
 Summary:
     This script produces plotly graphs visualizing my progress on the 10k mastery tracker project.
@@ -18,40 +18,65 @@ License:
     MIT
 
 Requires:
-    load_data, plotly
+    pandas, load_data, numpy, plotly
 
 Date Last Modified:
-	August 12, 2024
+	August 13, 2024
 """
 
+import pandas as pd
 from load_data import csvs_to_df
 import numpy as np
-import plotly.graph_objects as go 
+import plotly.graph_objects as go
 
 
 def visualize_data():
     df = csvs_to_df()
 
-    # Accumulate hours
-    df["hours_worked"] = np.floor(df["minutes_worked"] / 60)
+    df["hours_worked"] = np.round(df["minutes_worked"] / 60, 2)
     df["cumulative_hours"] = df["hours_worked"].cumsum()
+    df["date"] = pd.to_datetime(df["date"], format="%m-%d-%y")
+    cumulative_hours = df.groupby("date")["cumulative_hours"].max().reset_index()
 
-    # Create the figure
     fig = go.Figure()
 
-    # Add a trace
-    fig.add_trace(go.Scatter(x=df['date'], y=df['cumulative_hours'], mode='lines+markers'))
-
-    # Update layout for x-axis tickformat and hoverformat
-    fig.update_layout(
-        xaxis_title='Date',
-        yaxis_title='Value',
-        xaxis=dict(
-            tickformat='%m-%d-%y',  # Format for axis labels
-            hoverformat='%m-%d-%y'  # Format for hover tooltips
+    fig.add_trace(
+        go.Scatter(
+            x=cumulative_hours["date"],
+            y=cumulative_hours["cumulative_hours"],
+            name="Cumulative Hours",
+            marker_color="#00BFFF",
+            # hovertext=dict(x=cumulative),
+            # hoverinfo="text"
         )
     )
 
+    fig.add_trace(
+        go.Scatter(
+            x=df["date"],
+            y=df["hours_worked"],
+            name="Daily Hours",
+            marker_color="#333",
+        )
+    )
+
+    fig.update_layout(
+        xaxis_title="Date",
+        yaxis_title="Value",
+        title=dict(
+            text="Cumulative Hours Worked Over Time",
+            subtitle=dict(
+                text="Life expectancy by European country in 1952 and in 2002",
+                font=dict(color="gray"),
+            ),
+            y=0.9,
+            x=0.5,
+            xanchor="center",
+            yanchor="top",
+        ),
+        font=dict(family="Fire Code", color="#002F4C"),
+        xaxis_tickformat="%m-%d-%y",
+    )
     # Show the figure
     fig.show()
 
